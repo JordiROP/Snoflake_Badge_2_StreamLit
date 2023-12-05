c647e74d4be54f581d683f64e145a02eb39bf0df
Jordi Ricard Onrubia Palacios <jordirop@gmail.com>
2023-12-05 14:26:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import pandas
 import snowflake.connector
 import requests
 from urllib.error import URLError
+
+def get_fruityvice_data(fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_fruit)
+  return pandas.json_normalize(fruityvice_response.json())
+
 streamlit.title("My Parents New Healthy Dinner")
 
 streamlit.header("Breakfast Favorites")
@@ -25,12 +30,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(get_fruityvice_data(fruit_choice))
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -44,9 +44,7 @@ try:
   if not add_fruit:
     streamlit.error('Please select a fruit to get information')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_fruit)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(get_fruityvice_data(add_fruit))
 except URLError as e:
   streamlit.error()
   #streamlit.write('The user added ', add_fruit)
